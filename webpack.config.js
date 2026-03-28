216016e533d2f6c87d58e24fe5be2144aac83ffb
Yaroslav4tez <yaroslav4tez@gmail.com>
2026-03-28 16:01:30 -0500

    publicPath: '/home_work_32.1/'

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     filename: 'bundle.[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    publicPath: '/', 
+    publicPath: '/home_work_32.1/', 
   },
   module: {
     rules: [
